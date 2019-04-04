b6e6982d97f63d11d6368cc26c9da4ff3d54624b
Mike Rizzo <mjrizzo@gmail.com>
2019-04-04 16:01:38 -0400

    Added entry to __init__.py

--- humiominer/__init__.py
+++ humiominer/__init__.py
@@ -2,3 +2,8 @@ def prototypes():
     import os
 
     return os.path.join(os.path.dirname(__file__), 'prototypes')
+
+def webui_blueprint():
+    from minemeld.flask import aaa
+
+    return aaa.MMBlueprint('humio_search_miner_Webui', __name__, static_folder='webui', static_url_path='')
